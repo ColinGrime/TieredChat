a7f9d72767c49cc05086ade9af4d5687f3680415
Scill <realscill@gmail.com>
2022-03-09 16:03:17 -0500

    Add tab complete for channel names

--- src/main/java/com/github/scilldev/commands/chat/ChatBaseCommand.java
+++ src/main/java/com/github/scilldev/commands/chat/ChatBaseCommand.java
@@ -1,18 +1,39 @@
 package com.github.scilldev.commands.chat;
 
 import com.github.scilldev.TieredChat;
+import com.github.scilldev.chat.channel.ChatChannel;
 import com.github.scilldev.commands.BaseCommand;
 import com.github.scilldev.data.Messages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ChatBaseCommand extends BaseCommand {
 
+	private final TieredChat plugin;
+
 	public ChatBaseCommand(TieredChat plugin) {
 		super(plugin, "chat");
+		this.plugin = plugin;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		List<String> channels = new ArrayList<>();
+		for (ChatChannel channel : plugin.getChatManager().getChannels()) {
+			if (sender.hasPermission(channel.getPermission())) {
+				channels.add(channel.getName().toLowerCase());
+			}
+		}
+
+		return channels;
 	}
 
 	@Override
 	public Messages getUsage() {
-		return Messages.CHAT_USAGE;
+		return Messages.USAGE_CHAT_COMMAND;
 	}
 
 	@Override
