acfdfe6f6146a3e4706237713517a872beb877ee
Scill <realscill@gmail.com>
2022-03-08 22:41:27 -0500

    Reduce repeated code by adding it to BaseCommand.java

--- src/main/java/com/github/scilldev/commands/BaseCommand.java
+++ src/main/java/com/github/scilldev/commands/BaseCommand.java
@@ -4,11 +4,9 @@ import com.github.scilldev.TieredChat;
 import com.github.scilldev.data.Messages;
 import com.github.scilldev.utils.Logger;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class BaseCommand implements CommandExecutor, TabExecutor {
 
@@ -17,6 +15,7 @@ public abstract class BaseCommand implements CommandExecutor, TabExecutor {
 	public BaseCommand(TieredChat plugin, String name) {
 		PluginCommand command = plugin.getCommand(name);
 
+		// disable plugin if a command is invalid
 		if (command == null) {
 			Logger.severe("Commands have failed to load. Plugin has been disabled.");
 			plugin.getServer().getPluginManager().disablePlugin(plugin);
@@ -29,12 +28,35 @@ public abstract class BaseCommand implements CommandExecutor, TabExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// if there's no subcommand input, send the command usage
 		if (args.length == 0 || subCommands.get(args[0]) == null) {
 			getUsage().sendTo(sender);
 			return true;
 		}
 
-		onCommand(sender, args);
+		// if the console is both disabled and the sender, it's invalid
+		if (!allowConsole() && !(sender instanceof Player)) {
+			Messages.INVALID_SENDER.sendTo(sender);
+			return true;
+		}
+
+		SubCommand subCommand = getSubCommand(args[0]);
+		String permission = subCommand.getPermission();
+
+		// sender doesn't have permission
+		if (permission != null && !sender.hasPermission(permission)) {
+			Messages.INVALID_PERMISSION.sendTo(sender);
+			return true;
+		}
+
+		String[] subArgs = Arrays.copyOfRange(args, 1, args.length);
+		if (subCommand.requireArguments() && subArgs.length == 0) {
+			subCommand.getUsage().sendTo(sender);
+			return true;
+		}
+
+		// subcommand runs
+		subCommand.onCommand(sender, args[0], subArgs);
 		return true;
 	}
 
@@ -49,12 +71,16 @@ public abstract class BaseCommand implements CommandExecutor, TabExecutor {
 			return null;
 		}
 
-		return subCommand.onTabComplete(sender, args);
+		return subCommand.onTabComplete(sender, args[0], Arrays.copyOfRange(args, 1, args.length));
 	}
 
 	public void registerSubCommand(SubCommand subCommand) {
-		subCommands.put(subCommand.getName(), subCommand);
+		// register subcommand with name
+		if (subCommand.getName() != null) {
+			subCommands.put(subCommand.getName(), subCommand);
+		}
 
+		// register subcommand with aliases
 		if (subCommand.getAliases() != null) {
 			for (String alias : subCommand.getAliases()) {
 				subCommands.put(alias, subCommand);
@@ -66,7 +92,7 @@ public abstract class BaseCommand implements CommandExecutor, TabExecutor {
 		return subCommands.get(subCommandString);
 	}
 
-	public abstract void onCommand(CommandSender sender, String[] args);
-
 	public abstract Messages getUsage();
+
+	public abstract boolean allowConsole();
 }
--- src/main/java/com/github/scilldev/commands/chat/ChatBaseCommand.java
+++ src/main/java/com/github/scilldev/commands/chat/ChatBaseCommand.java
@@ -2,12 +2,7 @@ package com.github.scilldev.commands.chat;
 
 import com.github.scilldev.TieredChat;
 import com.github.scilldev.commands.BaseCommand;
-import com.github.scilldev.commands.SubCommand;
 import com.github.scilldev.data.Messages;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import java.util.Arrays;
 
 public final class ChatBaseCommand extends BaseCommand {
 
@@ -16,25 +11,12 @@ public final class ChatBaseCommand extends BaseCommand {
 	}
 
 	@Override
-	public void onCommand(CommandSender sender, String[] args) {
-		if (!(sender instanceof Player)) {
-			Messages.INVALID_SENDER.sendTo(sender);
-			return;
-		}
-
-		SubCommand subCommand = getSubCommand(args[0]);
-		String permission = subCommand.getPermission();
-
-		if (permission != null && !sender.hasPermission(permission)) {
-			Messages.INVALID_PERMISSION.sendTo(sender);
-			return;
-		}
-
-		subCommand.onCommand(sender, Arrays.copyOfRange(args, 1, args.length));
+	public Messages getUsage() {
+		return Messages.CHAT_USAGE;
 	}
 
 	@Override
-	public Messages getUsage() {
-		return Messages.CHAT_USAGE;
+	public boolean allowConsole() {
+		return false;
 	}
 }
