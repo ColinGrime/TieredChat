e2aa04f489bfc1401ec6cb04c646e83f13d255b6
Scill <realscill@gmail.com>
2022-03-08 22:47:54 -0500

    Add unique commands for each channel

--- src/main/java/com/github/scilldev/listeners/PlayerListeners.java
+++ src/main/java/com/github/scilldev/listeners/PlayerListeners.java
@@ -2,7 +2,6 @@ package com.github.scilldev.listeners;
 
 import com.github.scilldev.TieredChat;
 import com.github.scilldev.chat.channel.ChatChannel;
-import com.github.scilldev.chat.ChatUser;
 import com.github.scilldev.events.ChannelChatEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -10,6 +9,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.util.Set;
 
 public class PlayerListeners implements Listener {
 
@@ -23,19 +24,26 @@ public class PlayerListeners implements Listener {
 	@EventHandler
 	public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
 		Player player = event.getPlayer();
-		ChatUser user = plugin.getChatManager().getUser(player);
-		ChatChannel channel = user.getChannelPreference();
-
-		ChannelChatEvent channelEvent = new ChannelChatEvent(channel, user, event.getMessage(), channel.getAllowedListeners(user));
-		Bukkit.getScheduler().runTask(plugin, () -> Bukkit.getServer().getPluginManager().callEvent(channelEvent));
+		ChatChannel channel = plugin.getChatManager().getUser(player).getChannelPreference();
+		callChannelEvent(channel, player, event.getMessage(), channel.getAllowedListeners(player));
 
 		event.setCancelled(true);
 	}
 
 	@EventHandler
 	public void onCommandEvent(PlayerCommandPreprocessEvent event) {
-		// todo command stuff like /global
-		String commandName = event.getMessage().split(" ")[0];
-//		if (event.getMessage())
+		String commandName = event.getMessage().substring(1).split(" ")[0];
+		ChatChannel channel = plugin.getChatManager().getChannelByName(commandName);
+
+		if (channel != null) {
+			String message = event.getMessage().substring(commandName.length() + 1);
+			Player player = event.getPlayer();
+			callChannelEvent(channel, player, message, channel.getAllowedListeners(player));
+		}
+	}
+
+	private void callChannelEvent(ChatChannel channel, Player player, String message, Set<Player> recipients) {
+		ChannelChatEvent channelEvent = new ChannelChatEvent(channel, player, message, recipients);
+		Bukkit.getScheduler().runTask(plugin, () -> Bukkit.getServer().getPluginManager().callEvent(channelEvent));
 	}
 }
